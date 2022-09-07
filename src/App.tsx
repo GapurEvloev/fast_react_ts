2edd81f1cb977946d224ae6e712bc12f8ace276c
gapur <kaloycant@gmail.com>
2022-09-07 23:49:56 +0200

    create comp Product

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Product} from "./components/Product/Product";
+import {products} from "./data/products";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container m-auto">
+            { products.map(product => <Product product={product} key={product.id} />) }
+        </div>
+    )
 }
 
 export default App;
